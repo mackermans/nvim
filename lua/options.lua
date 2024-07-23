0d99f67cf597e9f739c0d869db20932eb992a55b
Maarten Ackermans <4571935+mackermans@users.noreply.github.com>
2024-07-23 22:08:27 +0100

    refactor: move mapleader to options

--- lua/options.lua
+++ lua/options.lua
@@ -1,3 +1,9 @@
+-- Set <space> as the leader key
+-- See `:help mapleader`
+--  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
 -- [[ Setting options ]]
 -- See `:help vim.opt`
 -- NOTE: You can change these options as you wish!
