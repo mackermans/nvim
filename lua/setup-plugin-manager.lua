6f48f06c0b70d7bfd91ae35ac556474e50d9da23
Maarten Ackermans <4571935+mackermans@users.noreply.github.com>
2024-07-23 21:56:32 +0100

    refactor: extract setup of lazy.nvim plugin manager

---
+++ lua/setup-plugin-manager.lua
@@ -0,0 +1,14 @@
+-- https://github.com/folke/lazy.nvim
+local function setup_plugin_manager()
+  local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+  if not vim.uv.fs_stat(lazypath) then
+    local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
+    local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
+    if vim.v.shell_error ~= 0 then
+      error('Error cloning lazy.nvim:\n' .. out)
+    end
+  end ---@diagnostic disable-next-line: undefined-field
+  vim.opt.rtp:prepend(lazypath)
+end
+
+setup_plugin_manager()
