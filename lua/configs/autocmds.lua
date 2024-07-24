1b36a280fab59e3232609a482e6167a38f35f441
Maarten Ackermans <4571935+mackermans@users.noreply.github.com>
2024-07-24 22:52:32 +0100

    feat: add autocmd to close some fts with q

--- lua/configs/autocmds.lua
+++ lua/configs/autocmds.lua
@@ -1,12 +1,42 @@
--- See `:help vim.highlight.on_yank()`
-local function register_autocmd_highlight_on_yank()
-  vim.api.nvim_create_autocmd('TextYankPost', {
-    desc = 'Highlight when yanking (copying) text',
-    group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
-    callback = function()
-      vim.highlight.on_yank()
-    end,
-  })
+local function augroup(name)
+  return vim.api.nvim_create_augroup('batman-' .. name, { clear = true })
 end
 
-register_autocmd_highlight_on_yank()
+-- highlight on yank
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = augroup 'highlight-yank',
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
+
+-- close some filetypes with <q>
+vim.api.nvim_create_autocmd('FileType', {
+  group = augroup 'close-with-q',
+  pattern = {
+    'PlenaryTestPopup',
+    'grug-far',
+    'help',
+    'lspinfo',
+    'notify',
+    'qf',
+    'spectre_panel',
+    'startuptime',
+    'tsplayground',
+    'neotest-output',
+    'checkhealth',
+    'neotest-summary',
+    'neotest-output-panel',
+    'dbout',
+    'gitsigns.blame',
+  },
+  callback = function(event)
+    vim.bo[event.buf].buflisted = false
+    vim.keymap.set('n', 'q', '<cmd>close<cr>', {
+      buffer = event.buf,
+      silent = true,
+      desc = 'Quit buffer',
+    })
+  end,
+})
