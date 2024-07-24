289a544fcef8bc1639c35083d29abdff1efe8054
Maarten Ackermans <4571935+mackermans@users.noreply.github.com>
2024-07-24 22:45:08 +0100

    refactor: move bufferline into interface plugin file

---
+++ lua/plugins/interface.lua
@@ -0,0 +1,12 @@
+return {
+  -- Shows buffers as tabs
+  -- https://github.com/akinsho/bufferline.nvim
+  {
+    'akinsho/bufferline.nvim',
+    version = '*',
+    dependencies = 'nvim-tree/nvim-web-devicons',
+    config = function()
+      require('bufferline').setup {}
+    end,
+  },
+}
