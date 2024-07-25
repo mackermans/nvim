cfa9ef20581f7d1cfdfea6d94539a9a32781ff88
Maarten Ackermans <4571935+mackermans@users.noreply.github.com>
2024-07-25 11:43:56 +0100

    refactor: move lazy.nvim setup to configs.lazy

--- lua/batman/configs/lazy.lua
+++ lua/batman/configs/lazy.lua
@@ -1,14 +1,55 @@
+-- Bootstrap lazy.nvim
 -- https://github.com/folke/lazy.nvim
-local function setup_plugin_manager()
-  local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
-  if not vim.uv.fs_stat(lazypath) then
-    local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
-    local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
-    if vim.v.shell_error ~= 0 then
-      error('Error cloning lazy.nvim:\n' .. out)
-    end
-  end ---@diagnostic disable-next-line: undefined-field
-  vim.opt.rtp:prepend(lazypath)
+local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+if not vim.uv.fs_stat(lazypath) then
+  local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
+  local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
+  if vim.v.shell_error ~= 0 then
+    error('Error cloning lazy.nvim:\n' .. out)
+  end
 end
+vim.opt.rtp:prepend(lazypath)
 
-setup_plugin_manager()
+-- Setup lazy.nvim
+require('lazy').setup {
+  spec = {
+    -- import plugins
+    -- require 'kickstart.plugins.debug',
+    -- require 'kickstart.plugins.indent_line',
+    -- require 'kickstart.plugins.lint',
+    -- require 'kickstart.plugins.autopairs',
+    { import = 'batman.plugins' },
+  },
+
+  -- automatically check for plugin updates
+  checker = { enabled = true },
+
+  -- customize icons
+  ui = {
+    icons = {
+      cmd = '⌘',
+      config = '🛠',
+      event = '📅',
+      favorite = ' ',
+      ft = '📂',
+      import = ' ',
+      init = '⚙',
+      keys = '🗝',
+      lazy = '💤 ',
+      list = {
+        '●',
+        '➜',
+        '★',
+        '‒',
+      },
+      loaded = '●',
+      not_loaded = '○',
+      plugin = '🔌',
+      runtime = '💻',
+      require = '🌙',
+      source = '📄',
+      start = '🚀',
+      task = '📌',
+    },
+  },
+}
