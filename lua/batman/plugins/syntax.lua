214ef8857b8378f2f731bf046ad3856ab554813c
Maarten Ackermans <4571935+mackermans@users.noreply.github.com>
2024-08-07 20:54:46 +0100

    feat(syntax): add better comments with ts-comments

--- lua/batman/plugins/syntax.lua
+++ lua/batman/plugins/syntax.lua
@@ -1,4 +1,12 @@
 return {
+  -- Enhance native comments with treesitter
+  {
+    'folke/ts-comments.nvim',
+    opts = {},
+    event = 'VeryLazy',
+    enabled = vim.fn.has 'nvim-0.10.0' == 1,
+  },
+
   -- Highlight, edit, and navigate code
   {
     'nvim-treesitter/nvim-treesitter',
