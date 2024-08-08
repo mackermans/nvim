0778d68040a84e561d7f9a7cfc1f3503c8fbe39a
Maarten Ackermans <4571935+mackermans@users.noreply.github.com>
2024-08-08 09:31:36 +0100

    feat(syntax): add color highlighter

--- lua/batman/plugins/syntax.lua
+++ lua/batman/plugins/syntax.lua
@@ -7,6 +7,36 @@ return {
     enabled = vim.fn.has 'nvim-0.10.0' == 1,
   },
 
+  -- Highlight colors
+  -- #FFF
+  -- #FF0000
+  -- #FF000055
+  -- rgb(0, 0, 255)
+  -- rgba(0, 255, 0, 0.5)
+  -- hsl(120, 100%, 70%)
+  -- text-indigo-500
+  {
+    'NvChad/nvim-colorizer.lua',
+    config = function()
+      require('colorizer').setup {
+        filetypes = { '*' },
+        user_default_options = {
+          RGB = true, -- #RGB hex codes
+          RRGGBB = true, -- #RRGGBB hex codes
+          names = true, -- "Name" codes like Blue
+          RRGGBBAA = true, -- #RRGGBBAA hex codes
+          rgb_fn = true, -- CSS rgb() and rgba() functions
+          hsl_fn = true, -- CSS hsl() and hsla() functions
+          css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+          css_fn = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
+          tailwind = true,
+          -- Available modes: foreground, background
+          mode = 'background', -- Set the display mode.
+        },
+      }
+    end,
+  },
+
   -- Show code context
   {
     'nvim-treesitter/nvim-treesitter-context',
