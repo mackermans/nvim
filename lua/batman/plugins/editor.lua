c278043f3f709199c24365a2ae5d982b196b88b0
Maarten Ackermans <4571935+mackermans@users.noreply.github.com>
2024-08-14 11:22:04 +0100

    feat(syntax): add treesitter textobjects and incremental select

--- lua/batman/plugins/editor.lua
+++ lua/batman/plugins/editor.lua
@@ -29,32 +29,6 @@ return {
     },
   },
 
-  -- Better Around/Inside textobjects
-  --  - va)  - [V]isually select [A]round [)]paren
-  --  - yilq - [Y]ank [I]nside [L]ast [Q]uote
-  --  - ci'  - [C]hange [I]nside [']quote
-  {
-    'echasnovski/mini.ai',
-    opts = {
-      mappings = {
-        -- Main textobject prefixes
-        around = 'a',
-        inside = 'i',
-
-        -- Next/last variants
-        around_next = 'an',
-        inside_next = 'in',
-        around_last = 'al',
-        inside_last = 'il',
-
-        -- Move cursor to corresponding edge of `a` textobject
-        goto_left = 'g[',
-        goto_right = 'g]',
-      },
-      n_lines = 500,
-    },
-  },
-
   -- File explorer
   {
     'echasnovski/mini.files',
--- lua/batman/plugins/syntax.lua
+++ lua/batman/plugins/syntax.lua
@@ -33,8 +33,27 @@ return {
   -- Show code context
   {
     'nvim-treesitter/nvim-treesitter-context',
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter',
+    },
     opts = {
       max_lines = 3,
+    },
+  },
+
+  -- Text objects navigation
+  {
+    'nvim-treesitter/nvim-treesitter-textobjects',
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter',
+    },
+  },
+
+  -- Text subjects navigation
+  {
+    'RRethy/nvim-treesitter-textsubjects',
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter',
     },
   },
 
@@ -91,6 +110,7 @@ return {
       ensure_installed = 'all',
       -- Autoinstall languages that are not installed
       auto_install = true,
+
       highlight = {
         enable = true,
         -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
@@ -98,22 +118,121 @@ return {
         --  the list of additional_vim_regex_highlighting and disabled languages for indent.
         additional_vim_regex_highlighting = { 'ruby' },
       },
+
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = '<CR>', -- set to `false` to disable one of the mappings
+          node_incremental = ']',
+          scope_incremental = 'grc',
+          node_decremental = '[',
+        },
+      },
+
       indent = { enable = true, disable = { 'ruby' } },
+
+      textobjects = {
+        select = {
+          enable = true,
+          lookahead = true,
+          keymaps = {
+            ['a='] = { query = '@assignment.outer', desc = 'Select outer part of an assignment' },
+            ['i='] = { query = '@assignment.inner', desc = 'Select inner part of an assignment' },
+            ['l='] = { query = '@assignment.lhs', desc = 'Select left hand side of an assignment' },
+            ['r='] = { query = '@assignment.rhs', desc = 'Select right hand side of an assignment' },
+
+            ['aa'] = { query = '@parameter.outer', desc = 'Select outer part of a parameter/argument' },
+            ['ia'] = { query = '@parameter.inner', desc = 'Select inner part of a parameter/argument' },
+
+            ['ai'] = { query = '@conditional.outer', desc = 'Select outer part of a conditional' },
+            ['ii'] = { query = '@conditional.inner', desc = 'Select inner part of a conditional' },
+
+            ['al'] = { query = '@loop.outer', desc = 'Select outer part of a loop' },
+            ['il'] = { query = '@loop.inner', desc = 'Select inner part of a loop' },
+
+            ['af'] = { query = '@call.outer', desc = 'Select outer part of a function call' },
+            ['if'] = { query = '@call.inner', desc = 'Select inner part of a function call' },
+
+            ['am'] = { query = '@function.outer', desc = 'Select outer part of a method/function definition' },
+            ['im'] = { query = '@function.inner', desc = 'Select inner part of a method/function definition' },
+
+            ['ac'] = { query = '@class.outer', desc = 'Select outer part of a class' },
+            ['ic'] = { query = '@class.inner', desc = 'Select inner part of a class' },
+          },
+        },
+        move = {
+          enable = true,
+          set_jumps = true, -- whether to set jumps in the jumplist
+          goto_next_start = {
+            [']f'] = { query = '@call.outer', desc = 'Next function call start' },
+            [']m'] = { query = '@function.outer', desc = 'Next method/function def start' },
+            [']c'] = { query = '@class.outer', desc = 'Next class start' },
+            [']i'] = { query = '@conditional.outer', desc = 'Next conditional start' },
+            [']l'] = { query = '@loop.outer', desc = 'Next loop start' },
+
+            -- You can pass a query group to use query from `queries/<lang>/<query_group>.scm file in your runtime path.
+            -- Below example nvim-treesitter's `locals.scm` and `folds.scm`. They also provide highlights.scm and indent.scm.
+            [']s'] = { query = '@scope', query_group = 'locals', desc = 'Next scope' },
+            [']z'] = { query = '@fold', query_group = 'folds', desc = 'Next fold' },
+          },
+          goto_next_end = {
+            [']F'] = { query = '@call.outer', desc = 'Next function call end' },
+            [']M'] = { query = '@function.outer', desc = 'Next method/function def end' },
+            [']C'] = { query = '@class.outer', desc = 'Next class end' },
+            [']I'] = { query = '@conditional.outer', desc = 'Next conditional end' },
+            [']L'] = { query = '@loop.outer', desc = 'Next loop end' },
+          },
+          goto_previous_start = {
+            ['[f'] = { query = '@call.outer', desc = 'Prev function call start' },
+            ['[m'] = { query = '@function.outer', desc = 'Prev method/function def start' },
+            ['[c'] = { query = '@class.outer', desc = 'Prev class start' },
+            ['[i'] = { query = '@conditional.outer', desc = 'Prev conditional start' },
+            ['[l'] = { query = '@loop.outer', desc = 'Prev loop start' },
+          },
+          goto_previous_end = {
+            ['[F'] = { query = '@call.outer', desc = 'Prev function call end' },
+            ['[M'] = { query = '@function.outer', desc = 'Prev method/function def end' },
+            ['[C'] = { query = '@class.outer', desc = 'Prev class end' },
+            ['[I'] = { query = '@conditional.outer', desc = 'Prev conditional end' },
+            ['[L'] = { query = '@loop.outer', desc = 'Prev loop end' },
+          },
+        },
+        lsp_interop = {
+          enable = true,
+          border = 'rounded',
+          peek_definition_code = {
+            ['gp'] = { query = '@function.outer', desc = '🌲Peek function definition' },
+            ['gcp'] = { query = '@class.outer', desc = '🌲Peek class definition' },
+          },
+        },
+        swap = {
+          enable = true,
+          swap_next = {
+            ['<leader>na'] = '@parameter.inner', -- swap parameters/argument with next
+            ['<leader>nm'] = '@function.outer', -- swap function with next
+          },
+          swap_previous = {
+            ['<leader>pa'] = '@parameter.inner', -- swap parameters/argument with prev
+            ['<leader>pm'] = '@function.outer', -- swap function with previous
+          },
+        },
+      },
+
+      textsubjects = {
+        enable = true,
+        prev_selection = ',', -- (Optional) keymap to select the previous selection
+        keymaps = {
+          ['<CR>'] = { 'textsubjects-smart', desc = 'Smart-select containers (classes, functions, etc.)' },
+          ['a;'] = { 'textsubjects-container-outer', desc = 'Select outside containers (classes, functions, etc.)' },
+          ['i;'] = { 'textsubjects-container-inner', desc = 'Select inside containers (classes, functions, etc.)' },
+        },
+      },
     },
     config = function(_, opts)
-      -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
-
       -- Prefer git instead of curl in order to improve connectivity in some environments
       require('nvim-treesitter.install').prefer_git = true
       ---@diagnostic disable-next-line: missing-fields
       require('nvim-treesitter.configs').setup(opts)
-
-      -- There are additional nvim-treesitter modules that you can use to interact
-      -- with nvim-treesitter. You should go explore a few and see what interests you:
-      --
-      --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
-      --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
-      --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
     end,
   },
 }
