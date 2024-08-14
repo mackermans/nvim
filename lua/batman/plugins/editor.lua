ba80fab854936abfdb9fdddc6d9f278d0a3d9b95
Maarten Ackermans <4571935+mackermans@users.noreply.github.com>
2024-08-14 11:22:04 +0100

    feat(editor): tmux/nvim pane navigation

--- lua/batman/plugins/editor.lua
+++ lua/batman/plugins/editor.lua
@@ -138,6 +138,24 @@ return {
     },
   },
 
+  -- Seamless Tmux/Nvim pane navigation
+  {
+    'alexghergh/nvim-tmux-navigation',
+    config = function()
+      require('nvim-tmux-navigation').setup {
+        disable_when_zoomed = true, -- defaults to false
+        keybindings = {
+          left = '<C-h>',
+          down = '<C-j>',
+          up = '<C-k>',
+          right = '<C-l>',
+          last_active = '<C-\\>',
+          next = '<C-Space>',
+        },
+      }
+    end,
+  },
+
   -- Shows pending keybinds
   {
     'folke/which-key.nvim',
