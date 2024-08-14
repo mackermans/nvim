a3bcc84e596ac9806daa36e7cae036c6f7f38f61
Maarten Ackermans <4571935+mackermans@users.noreply.github.com>
2024-08-14 11:22:04 +0100

    feat(ai): add avante plugin

--- lua/batman/plugins/ai.lua
+++ lua/batman/plugins/ai.lua
@@ -20,4 +20,44 @@ return {
       end,
     },
   },
+
+  {
+    'yetone/avante.nvim',
+    event = 'VeryLazy',
+    build = 'make', -- This is Optional, only if you want to use tiktoken_core to calculate tokens count
+    opts = {
+      provider = 'copilot',
+    },
+    dependencies = {
+      'stevearc/dressing.nvim',
+      'nvim-lua/plenary.nvim',
+      'MunifTanjim/nui.nvim',
+      --- The below dependencies are optional,
+      'nvim-tree/nvim-web-devicons', -- or echasnovski/mini.icons
+      'zbirenbaum/copilot.lua', -- for providers='copilot'
+      {
+        -- support for image pasting
+        'HakonHarnes/img-clip.nvim',
+        event = 'VeryLazy',
+        opts = {
+          -- recommended settings
+          default = {
+            embed_image_as_base64 = false,
+            prompt_for_file_name = false,
+            drag_and_drop = {
+              insert_mode = true,
+            },
+          },
+        },
+      },
+      {
+        -- Make sure to set this up properly if you have lazy=true
+        'MeanderingProgrammer/render-markdown.nvim',
+        opts = {
+          file_types = { 'markdown', 'Avante' },
+        },
+        ft = { 'markdown', 'Avante' },
+      },
+    },
+  },
 }
