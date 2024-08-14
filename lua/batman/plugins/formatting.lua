6aa4bd1a141a7133460629204f1e1c8e5210f01b
Maarten Ackermans <4571935+mackermans@users.noreply.github.com>
2024-08-14 11:22:04 +0100

    feat(formatting): use prettier

--- lua/batman/plugins/formatting.lua
+++ lua/batman/plugins/formatting.lua
@@ -12,26 +12,28 @@ return {
       desc = '[C]ode [F]ormat buffer',
     },
   },
+  ---@module "conform"
+  ---@type conform.setupOpts
   opts = {
     notify_on_error = false,
     format_on_save = function(bufnr)
-      -- Disable "format_on_save lsp_fallback" for languages that don't
-      -- have a well standardized coding style. You can add additional
-      -- languages here or re-enable it for the disabled ones.
-      local disable_filetypes = { c = true, cpp = true }
       return {
+        async = false,
+        lsp_format = 'fallback',
         timeout_ms = 1000,
-        -- lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
       }
     end,
+
     formatters_by_ft = {
+      css = { 'prettier' },
+      html = { 'prettier' },
+      json = { 'prettier' },
+      jsonc = { 'prettier' },
       lua = { 'stylua' },
-      -- Conform can also run multiple formatters sequentially
-      -- python = { "isort", "black" },
-      --
-      -- You can use a sub-list to tell conform to run *until* a formatter
-      -- is found.
-      -- javascript = { { "prettierd", "prettier" } },
     },
   },
+  init = function()
+    -- If you want the formatexpr, here is the place to set it
+    vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+  end,
 }
