b9de1a6081b8510cb466c1b2d28315613af58c56
Maarten Ackermans <4571935+mackermans@users.noreply.github.com>
2024-08-14 11:22:04 +0100

    feat(interface): add edgy plugin

--- lua/batman/plugins/interface.lua
+++ lua/batman/plugins/interface.lua
@@ -5,9 +5,102 @@ return {
     'akinsho/bufferline.nvim',
     version = '*',
     dependencies = 'nvim-tree/nvim-web-devicons',
+    opts = function()
+      local Offset = require 'bufferline.offset'
+      if not Offset.edgy then
+        local get = Offset.get
+        Offset.get = function()
+          if package.loaded.edgy then
+            local layout = require('edgy.config').layout
+            local ret = { left = '', left_size = 0, right = '', right_size = 0 }
+            for _, pos in ipairs { 'left', 'right' } do
+              local sb = layout[pos]
+              if sb and #sb.wins > 0 then
+                local title = ' Sidebar' .. string.rep(' ', sb.bounds.width - 8)
+                ret[pos] = '%#EdgyTitle#' .. title .. '%*' .. '%#WinSeparator#│%*'
+                ret[pos .. '_size'] = sb.bounds.width
+              end
+            end
+            ret.total_size = ret.left_size + ret.right_size
+            if ret.total_size > 0 then
+              return ret
+            end
+          end
+          return get()
+        end
+        Offset.edgy = true
+      end
+    end,
     config = function()
       require('bufferline').setup {}
     end,
+  },
+
+  -- Window manager
+  {
+    'folke/edgy.nvim',
+    event = 'VeryLazy',
+    opts = {
+      -- edgebar animations
+      animate = {
+        enabled = true,
+        fps = 100, -- frames per second
+        cps = 120, -- cells per second
+        on_begin = function()
+          vim.g.minianimate_disable = true
+        end,
+        on_end = function()
+          vim.g.minianimate_disable = false
+        end,
+        -- Spinner for pinned views that are loading.
+        -- if you have noice.nvim installed, you can use any spinner from it, like:
+        -- spinner = require("noice.util.spinners").spinners.circleFull,
+        spinner = {
+          frames = { '⠋', '⠙', '⠹', '⠸', '⠼', '⠴', '⠦', '⠧', '⠇', '⠏' },
+          interval = 80,
+        },
+      },
+      bottom = {
+        -- toggleterm / lazyterm at the bottom with a height of 40% of the screen
+        {
+          ft = 'toggleterm',
+          size = { height = 0.2 },
+          -- exclude floating windows
+          filter = function(buf, win)
+            return vim.api.nvim_win_get_config(win).relative == ''
+          end,
+        },
+        {
+          ft = 'lazyterm',
+          title = 'LazyTerm',
+          size = { height = 0.4 },
+          filter = function(buf)
+            return not vim.b[buf].lazyterm_cmd
+          end,
+        },
+        'Trouble',
+        { ft = 'qf', title = 'QuickFix' },
+        {
+          ft = 'help',
+          size = { height = 20 },
+          -- only show help buffers
+          filter = function(buf)
+            return vim.bo[buf].buftype == 'help'
+          end,
+        },
+      },
+      left = {
+        {
+          title = function()
+            local buf_name = vim.api.nvim_buf_get_name(0) or '[No Name]'
+            return vim.fn.fnamemodify(buf_name, ':t')
+          end,
+          ft = 'Outline',
+          pinned = true,
+          open = 'SymbolsOutlineOpen',
+        },
+      },
+    },
   },
 
   -- Statusline
