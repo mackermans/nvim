f22e96e5215a9021a42b284cde5e2f4b211e8d1f
Maarten Ackermans <4571935+mackermans@users.noreply.github.com>
2024-08-14 11:22:04 +0100

    feat(system): add flatten.nvim plugin singleton neovim instance

---
+++ lua/batman/plugins/system.lua
@@ -0,0 +1,9 @@
+return {
+  -- Open files and command output from `:term`, Wezterm and Kitty in your current neovim instance
+  {
+    'willothy/flatten.nvim',
+    config = true,
+    lazy = false,
+    priority = 1001,
+  },
+}
